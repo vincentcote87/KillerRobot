c42d8f78d6d391fa679487ec85d0bd5ca0f41d05
Vincent <vincentcote87@gmail.com>
2019-03-14 14:19:42 -0600

    Added skeleton file to start

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <GL/glu.h>
+
+int window_width = 800;
+int window_height = 600;
+int window_position_x = 100;
+int window_position_y = 100;
+
+void display(void) {
+  // Add display functions here
+}
+
+void reshape(int w, int h) {
+  // Add reshape functions here
+}
+
+void init(void) {
+  // Add init functions here
+  glClearColor (0.0, 0.0, 0.0, 0.0);
+}
+
+void Keyboard(unsigned char key, int x, int y) {
+  // Add regular keyboard functions here
+}
+
+void specialKeyboard(int key, int x, int y) {
+  // Add special keyboard functions here
+}
+
+void mouse(int button, int state, int x, int y) {
+  // Add mouse functions here
+}
+
+int main(int argc, char** argv) {
+  
+  glutInit(&argc, argv);
+  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize (window_width, window_height); 
+  glutInitWindowPosition (window_position_x, window_position_y);
+  glutCreateWindow ("Killer Robot");
+  init ();
+  glutDisplayFunc(display); 
+  glutReshapeFunc(reshape); 
+  glutKeyboardFunc(keyboard);
+  glutSpecialFunc(specialKeyboard);
+  glutMouseFunc(mouse);
+  glutMainLoop();
+  
+  return 0;
+}
