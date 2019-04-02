a9c601d8ed317f5de62078cc74ec5eb0d64d6331
darshthanki <darsh.thanki@wbms.ca>
2019-04-02 12:46:18 -0600

    No change...

--- buildings/building.c
+++ buildings/building.c
@@ -173,7 +173,7 @@ void Building::Destroy()
 {
    if(hitCount == 0){
      base -= 10000;
-    //hitCount--; //Simply redraw the building in the opposite direction. Essentially hiding the building.
+     //hitCount--; //Simply redraw the building in the opposite direction. Essentially hiding the building.
   }else{
     hitCount--;
   }
